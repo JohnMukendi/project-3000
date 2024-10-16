d1eaf03c57631889d9489a558ecf14f45b795ca3
JohnMukendi <mukendijohn937@gmail.com>
2024-10-16 15:39:57 +0200

    changes made

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/users", (req, res) => {
-  res.send(200).json([{ name: "John" }, { name: "Nicholas Cage" }]);
+  res.json([{ name: "John" }, { name: "Nicholas Cage" }]);
 });
 app.listen(port, () => {
   console.log("server running on port " + port);
